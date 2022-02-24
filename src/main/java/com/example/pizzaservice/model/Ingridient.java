99391e1a167e2bba415a5d540ef7fc23ffb03be1
Borys <boria81@gmail.com>
2022-02-24 23:04:05 +0000

    Model for Ingridient

---
+++ src/main/java/com/example/pizzaservice/model/Ingridient.java
@@ -0,0 +1,14 @@
+package com.example.pizzaservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingridient {
+
+    private String name;
+    private Double price;
+}
